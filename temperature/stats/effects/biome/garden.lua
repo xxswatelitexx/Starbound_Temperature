b6977d3af915eb1ad3c88ad9301852daefc5031f
Mr. Kal <xpiratosx@gmail.com>
2015-01-27 22:05:02 +0530

    Removed some unnecessary code added torch

--- temperature/stats/effects/biome/garden.lua
+++ temperature/stats/effects/biome/garden.lua
@@ -1,11 +1,11 @@
 function init()
 	script.setUpdateDelta(5)
 	-- Config Files --
-	self.biomeLow = effect.configParameter("biomeTempLow", 4500)
-	self.biomeHigh = effect.configParameter("biomeTempHigh", 4500)  
+	--self.biomeLow = effect.configParameter("biomeTempLow", 4500)
+	--self.biomeHigh = effect.configParameter("biomeTempHigh", 4500)  
 	self.biomeTempRate = effect.configParameter("biomeTempRatePerSec", 0)
 	--Calculations--
-	self.biomeTemp = 200
+	self.biomeTemp = effect.configParameter("biomeTempLow", 4500)
 	--biomeTemp = math.random(effect.configParameter("biomeTempLow", 1000),effect.configParameter("biomeTempHigh", 1000) )
   world.setProperty("temperature", biomeTemp)
 	--self.deltaTemperature = (status.stat("rateTemperature") / (effect.duration() * dt)) /2
@@ -27,11 +27,8 @@ function update(dt)
     self.tickTimer = self.tickTime
     if status.resource("temperature") >= self.biomeTemp then 
 		status.modifyResource("temperature", self.biomeTempRate + status.resource("armorCold")) 
-		--world.logInfo(tostring(status.resource("temperature")).." Temperature")
+		world.logInfo(tostring(status.resource("temperature")).." Temperature")
 		--world.logInfo(tostring(self.deltaTemperature + status.resource("armorCold")).." Biome Modifier Calculation")
-		else
-		effect.addStatModifierGroup({{stat = "rateTemperature", amount = -(status.stat("rateTemperature"))}}) 
-		--world.logInfo("Calculation 2 working Temperature")
 		end
   end
 end
---
+++ temperature/stats/effects/temperature_modifierObject/temperature_modifierObject.lua
@@ -0,0 +1,24 @@
+function init()
+	script.setUpdateDelta(5)
+	self.temperatureMod = effect.configParameter("tempModifier", 0) / effect.duration()
+	self.temperatureLimit = effect.configParameter("tempLimiter", 0)
+	self.tickTimer = 1
+end
+
+function update(dt)
+	if status.resource("temperature") <= 9000 then
+	status.modifyResource("temperature", self.temperatureMod * dt)
+	self.tickTimer = self.tickTimer - dt
+	end
+	self.tickTimer = self.tickTimer - dt
+	if self.tickTimer <= 0 then
+	self.tickTimer = 1
+    world.logInfo(status.resource("temperature").." Temperature after Effect")
+		world.logInfo(tostring(self.temperatureMod * dt).." Effect Calculation")
+  end
+	
+end
+
+function uninit()
+
+end
